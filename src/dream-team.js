6f82654d5b802120fcf927351030907f315921a7
Anastasiya Zarubina <71637520+stacyzar@users.noreply.github.com>
2021-03-25 15:28:26 +0500

    feat: fix solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) return false;
   return members.map(name => {
     if (typeof name === 'string') return name.trim()[0].toUpperCase();
