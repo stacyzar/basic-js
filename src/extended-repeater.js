b960a84972afe3973fad4cb128038983dc6ed825
Anastasiya Zarubina <71637520+stacyzar@users.noreply.github.com>
2021-03-27 11:08:18 +0500

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,2 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
-  let { repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|'} = options;
-  
-        if (typeof (str) !== 'string ) {
-            String(str)
-        }
-        
-        if (addition === undefined) {
-             return (str + separator)
-             .repeat(repeatTimes)
-             .slice(0, -separator.length);
-        }
-        
-        if (typeof (addition) !== 'string') {
-             String(addition)
-        }
-        
-        return (str + (addition + additionSeparator)
-             .repeat(additionRepeatTimes)
-             .slice(0, -additionSeparator.length) + separator)
-             .repeat(repeatTimes);
-};
   
