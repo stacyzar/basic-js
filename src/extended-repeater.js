57a8a246b37e1366cf40003a928fdc4f552920ea
Anastasiya Zarubina <71637520+stacyzar@users.noreply.github.com>
2021-03-27 10:49:36 +0500

    feat: make a solution for task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|'} = options;
+  
+        if (typeof (str) !== 'string ) {
+            String(str)
+        }
+        
+        if (addition === undefined) {
+             return (str + separator)
+             .repeat(repeatTimes)
+             .slice(0, -separator.length);
+        }
+        
+        if (typeof (addition) !== 'string') {
+             String(addition)
+        }
+        
+        return (str + (addition + additionSeparator)
+             .repeat(additionRepeatTimes)
+             .slice(0, -additionSeparator.length) + separator)
+             .repeat(repeatTimes)
+             .slice(0, -separator.length);
 };
   
