6ba927f901e40034e0e3da66d3638a92757b02ee
Anastasiya Zarubina <71637520+stacyzar@users.noreply.github.com>
2021-03-28 19:55:49 +0500

    fix: update new solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,2 +1,20 @@
+const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+    if (!(str || options)) return false
+    let {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|',
+    } = options
 
+    str = String(str)
+    addition = String(addition)
+
+    let additionsString = new Array(additionRepeatTimes).fill(addition).join(additionSeparator)
+    let fullString = new Array(repeatTimes).fill(str + additionsString).join(separator)
+
+    return fullString
+}
   
